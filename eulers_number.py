28502a242dabb9501ffabcf9a4df43445ba84d56
LJ Brown <browlm13@gmail.com>
2018-11-24 05:14:28 -0600

    Update eulers_number.py

--- eulers_number.py
+++ eulers_number.py
@@ -42,9 +42,11 @@
 __author__  = "LJ Brown"
 __file__ = "eulers_number.py"
 
+from decimal import *
+
 def an(n):
-	""" a(n) = n*a(n-1) + 1, a(0) = 0 """
-	an = 0
+	""" a(n) = n*a(n-1) + 1, a(0) = 1 """
+	an = 1
 	for i in range(1,n):
 		an = i*an + 1
 	return an
@@ -61,12 +63,31 @@ def eulers(t):
 		v *= vi
 	return at/v
 
-if __name__ == "__main__":
-	# Mathmatica
-	e_true = 2.718281828459045
+def eulers_precise(t, prec=100):
+	getcontext().prec = prec
+	v, at = Decimal(1.0), Decimal(1.0)
+	for i in range(1,t+1):
+		vi = i
+		at = vi * at + Decimal(1.0)
+		v *= vi
+	return at/v
 
+
+if __name__ == "__main__":
+
+	# Mathmatica
+	e_true = 2.71828182845
 	t = 17 # 52 total arithmetic operations, 16 d.p.a
 	e_hat = eulers(t)
 	error = abs(e_hat - e_true)
+	print("t=%s:\n\ttrue value = %s\n\tapproximation = %s,\n\terror = %s" % (t, e_true, e_hat, error))
 
+	# higher precision
+	t = 178
+	prec = 179
+	e_hat = eulers_precise(t,prec)
+	getcontext().prec = prec
+	# Mathmatica
+	e_true = Decimal('2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059921817413596629043572900334295260595630738132328627943490763')
+	error = e_hat - e_true
 	print("t=%s:\n\ttrue value = %s\n\tapproximation = %s,\n\terror = %s" % (t, e_true, e_hat, error))
